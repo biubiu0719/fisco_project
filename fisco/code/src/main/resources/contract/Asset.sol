3e03b072274496c7df5e43e23c2bb2706a6766f3
biubiu0719 <1317692869@qq.com>
2021-01-29 03:24:39 +0800

    update

--- fisco/code/src/main/resources/contract/Asset.sol
+++ fisco/code/src/main/resources/contract/Asset.sol
@@ -1,77 +1,102 @@
-pragma solidity >=0.4.24 <0.6.11;
+pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
 
 import "./Table.sol";
 
 contract Asset {
     // event
-    event RegisterEvent(int256 ret, string indexed account, uint256 indexed asset_value);
-    event TransferEvent(int256 ret, string indexed from_account, string indexed to_account, uint256 indexed amount);
-    
+    event RegisterEvent(int256 ret, string account, int256 asset_value);
+    event TransferEvent(int256 ret, string from_account, string to_account, int256 amount);
+    event AddTransactionEvent(int256 ret, string id, string acc1, string acc2, int256 money);
+    event UpdateTransactionEvent(int256 ret, string id, int256 money);
+
     constructor() public {
         // 构造函数中创建t_asset表
         createTable();
     }
 
     function createTable() private {
-        TableFactory tf = TableFactory(0x1001); 
-        // 资产管理表, key : account, field : asset_value
-        // |  资产账户(主键)      |     资产金额       |
-        // |-------------------- |-------------------|
-        // |        account      |    asset_value    |     
-        // |---------------------|-------------------|
-        //
+        TableFactory tf = TableFactory(0x1001);
         // 创建表
         tf.createTable("t_asset", "account", "asset_value");
+        // 交易记录表, key: id, field: acc1, acc2, money, status
+        tf.createTable("t_transaction", "id","acc1, acc2, money, status");
     }
 
-    function openTable() internal returns(Table) {
+    // 返回t_asset
+    function openAssetTable() private returns(Table) {
         TableFactory tf = TableFactory(0x1001);
         Table table = tf.openTable("t_asset");
         return table;
     }
 
-    /*
-    描述 : 根据资产账户查询资产金额
-    参数 ： 
-            account : 资产账户
-
-    返回值：
-            参数一： 成功返回0, 账户不存在返回-1
-            参数二： 第一个参数为0时有效，资产金额
-    */
-    function select(string memory account) public view returns(int256, uint256) {
-        // 打开表
+    // 返回t_transaction
+    function openTransactionTable() private returns(Table) {
         TableFactory tf = TableFactory(0x1001);
-        Table table = tf.openTable("t_asset");
+        Table table = tf.openTable("t_transaction");
+        return table;
+    }
+
+    /*
+    查询信用金额
+
+        account : 资产账户
+
+    */
+    function select(string account) public constant returns(int256, int256) {
+        // 打开表
+        Table table = openAssetTable();
         // 查询
         Entries entries = table.select(account, table.newCondition());
-        uint256 asset_value = 0;
+        int256 asset_value = 0;
         if (0 == uint256(entries.size())) {
             return (-1, asset_value);
         } else {
             Entry entry = entries.get(0);
-            return (0, uint256(entry.getInt("asset_value")));
+            return (0, int256(entry.getInt("asset_value")));
         }
     }
 
     /*
-    描述 : 资产注册
-    参数 ： 
-            account : 资产账户
-            amount  : 资产金额
-    返回值：
-            0  资产注册成功
-            -1 资产账户已存在
-            -2 其他错误
+    查询欠条信息
+            id : 欠条id号
     */
-    function register(string memory account, uint256 asset_value) public returns(int256){
+    function select_transaction(string id) public constant returns(int256[], bytes32[]) {
+        // 打开表
+        Table table = openTransactionTable();
+        // 查询
+        Entries entries = table.select(id, table.newCondition());
+        //         bytes32[] memory str_list = new bytes32[](2);
+
+        int256[] memory int_list = new int256[](3);   //ret_code, money, status
+        bytes32[] memory str_list = new bytes32[](2);   //acc1, acc2
+        if (0 == uint256(entries.size())) {
+            int_list[0] = -1;
+            return (int_list, str_list);
+        } else {
+            Entry entry = entries.get(0);
+            int_list[1] = entry.getInt("money");
+            int_list[2] = entry.getInt("status");
+            str_list[0] = entry.getBytes32("acc1");
+            str_list[1] = entry.getBytes32("acc2");
+            return (int_list, str_list);
+        }
+    }
+
+    /*
+    注册账户
+            account : 账户名
+            amount  : 额度
+    */
+    function register(string account, int256 asset_value) public returns(int256){
         int256 ret_code = 0;
-        int256 ret= 0;
-        uint256 temp_asset_value = 0;
-        // 查询账号是否存在
+        int256 ret = 0;
+        int256 temp_asset_value = 0;
+        // 查询账户是否存在
+
         (ret, temp_asset_value) = select(account);
         if(ret != 0) {
-            Table table = openTable();
+            Table table = openAssetTable();
             
             Entry entry = table.newEntry();
             entry.set("account", account);
@@ -96,27 +121,132 @@ contract Asset {
     }
 
     /*
-    描述 : 资产转移
+    借钱（打欠条）
     参数 ： 
+            id : 欠条号
+            zhaizhu : 债主
+            account2 : 借债人
+            money: 金额
+    */
+    function addTransaction(string id, string zhaizhu, string account2, int256 money) public returns(int256){
+        int256 ret_code = 0;
+        int256 ret = 0;
+        bytes32[] memory str_list = new bytes32[](2);
+        int256[] memory int_list = new int256[](3);
+        
+        // 查询交易是否存在
+        (int_list, str_list) = select_transaction(id);
+        if(int_list[0] != int256(0)) {
+            Table table = openTransactionTable();
+
+            Entry entry0 = table.newEntry();
+            entry0.set("id", id);
+            entry0.set("acc1", zhaizhu);
+            entry0.set("acc2", account2);
+            entry0.set("money", int256(money));
+            entry0.set("status", int256(money));
+            // 插入
+            int count = table.insert(id, entry0);
+            if (count == 1) {
+                // 将欠款人的信用额度转移一部分给债主
+                ret = transfer(account2,zhaizhu,money);
+                // 信用额度转让失败
+                if(ret != 0) {
+                    ret_code = -3;
+                } else {
+                    ret_code = 0;
+                }
+            } else {
+                // 失败? 无权限或者其他错误
+                ret_code = -2;
+            }
+        } else {
+            // 交易ID已存在
+            ret_code = -1;
+        }
+
+        emit AddTransactionEvent(ret_code, id, zhaizhu, account2, money);
+
+        return ret_code;
+    }
+
+    /*
+            支付欠条
+            id : 欠条号码
+            money: 金额
+    */
+    function updateTransaction(string id, int256 money) public returns(int256, string[]){
+        int256 ret_code = 0;
+        // int256 ret = 0;
+        bytes32[] memory str_list = new bytes32[](2);
+        int256[] memory int_list = new int256[](3);
+        string[] memory acc_list = new string[](2);
+        // 查询该欠条是否存在
+        (int_list, str_list) = select_transaction(id);
+        acc_list[0] = byte32ToString(str_list[0]);
+        acc_list[1] = byte32ToString(str_list[1]);
+
+        if(int_list[0] == 0) { // 交易ID存在
+
+            // 当还的钱大于应还钱数目的时候，返回-2
+            if(int_list[2] < money){
+                ret_code = -2;
+                emit UpdateTransactionEvent(ret_code, id, money);
+                return (ret_code, acc_list);
+            }
+
+            // 更新交易状态
+            Table table = openTransactionTable();
+
+            Entry entry0 = table.newEntry();
+            entry0.set("id", id);
+            entry0.set("acc1", byte32ToString(str_list[0]));
+            entry0.set("acc2", byte32ToString(str_list[1]));
+            entry0.set("money", int_list[1]);
+            entry0.set("status", (int_list[2] - money));
+
+            // 更新欠条
+            int count = table.update(id, entry0, table.newCondition());
+            if(count != 1) {
+                ret_code = -3;
+                // 失败? 无权限或者其他错误?
+                emit UpdateTransactionEvent(ret_code, id, money);
+                return (ret_code,acc_list);
+            }
+
+            // 信用额度返还
+            //由于还钱之后，还债人的额度能够增加
+            int256 temp = transfer(byte32ToString(str_list[0]),byte32ToString(str_list[1]),money);
+            if(temp != 0){
+                ret_code = -4 * 10 + temp;
+                emit UpdateTransactionEvent(ret_code, id, money);
+                return (ret_code,acc_list);
+            }
+
+            ret_code = 0;
+      
+        } else { // 交易ID不存在
+            ret_code = -1;
+        }
+        emit UpdateTransactionEvent(ret_code, id, money);
+
+        return (ret_code,acc_list);
+    }
+
+    /*
+    信用额度转移
             from_account : 转移资产账户
             to_account ： 接收资产账户
             amount ： 转移金额
-    返回值：
-            0  资产转移成功
-            -1 转移资产账户不存在
-            -2 接收资产账户不存在
-            -3 金额不足
-            -4 金额溢出
-            -5 其他错误
     */
-    function transfer(string memory from_account, string memory to_account, uint256 amount) public returns(int256) {
+    function transfer(string from_account, string to_account, int256 amount) public returns(int256) {
         // 查询转移资产账户信息
         int ret_code = 0;
         int256 ret = 0;
-        uint256 from_asset_value = 0;
-        uint256 to_asset_value = 0;
+        int256 from_asset_value = 0;
+        int256 to_asset_value = 0;
         
-        // 转移账户是否存在?
+        // 查询from_account是否合法
         (ret, from_asset_value) = select(from_account);
         if(ret != 0) {
             ret_code = -1;
@@ -126,7 +256,7 @@ contract Asset {
 
         }
 
-        // 接受账户是否存在?
+        // 查询to_account是否合法
         (ret, to_asset_value) = select(to_account);
         if(ret != 0) {
             ret_code = -2;
@@ -137,24 +267,18 @@ contract Asset {
 
         if(from_asset_value < amount) {
             ret_code = -3;
-            // 转移资产的账户金额不足
+            // 当from_account的额度不足，则返回-3
             emit TransferEvent(ret_code, from_account, to_account, amount);
             return ret_code;
         } 
 
-        if (to_asset_value + amount < to_asset_value) {
-            ret_code = -4;
-            // 接收账户金额溢出
-            emit TransferEvent(ret_code, from_account, to_account, amount);
-            return ret_code;
-        }
 
-        Table table = openTable();
+        Table table = openAssetTable();
 
         Entry entry0 = table.newEntry();
         entry0.set("account", from_account);
         entry0.set("asset_value", int256(from_asset_value - amount));
-        // 更新转账账户
+        // 更新from_account的额度
         int count = table.update(from_account, entry0, table.newCondition());
         if(count != 1) {
             ret_code = -5;
@@ -166,11 +290,10 @@ contract Asset {
         Entry entry1 = table.newEntry();
         entry1.set("account", to_account);
         entry1.set("asset_value", int256(to_asset_value + amount));
-        // 更新接收账户
+        // 更新to_account的额度
         table.update(to_account, entry1, table.newCondition());
 
         emit TransferEvent(ret_code, from_account, to_account, amount);
 
         return ret_code;
     }
-}
